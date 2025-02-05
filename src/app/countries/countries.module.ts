a392d84d79050d7fa48a0a8577155a8e26e7b91b
Christopher Riquelme <ccriquelmes@gmail.com>
2025-02-04 23:56:15 -0300

    CountryTable Component

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -6,6 +6,7 @@ import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.com
 import { CountriesRoutingModule } from './countries-routing.module';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from '../shared/shared.module';
     ByCountryPageComponent,
     ByRegionPageComponent,
     CountryPageComponent,
+    CountryTableComponent,
   ],
   imports: [CommonModule, CountriesRoutingModule, SharedModule],
 })
