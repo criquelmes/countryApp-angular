66024cbb5271b3d49d1c9943b196c4438f8c972a
Christopher Riquelme <ccriquelmes@gmail.com>
2025-02-06 23:58:10 -0300

    Sección 9: Argumentos por URL - Página de país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country-page',
   standalone: false,
-  
-  templateUrl: './country-page.component.html',
-  styleUrl: './country-page.component.css'
-})
-export class CountryPageComponent {
 
+  templateUrl: './country-page.component.html',
+  styleUrl: './country-page.component.css',
+})
+export class CountryPageComponent implements OnInit {
+  constructor(activatedRoute: ActivatedRoute) {}
 }
